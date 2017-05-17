830b04b8c38b9c48a3b2fe3271ddb33a04f8b476
ADL175 <armydavidlim@gmail.com>
2017-05-17 13:03:48 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="trigrams",
+    description="A Python program",
+    version="0.1",
+    author="David Lim",
+    author_email="armydavidlim@gmail.com",
+    license="MIT",
+    py_modules=['trigrams'],
+    package_dir={'': 'src'},
+    install_requires=[],
+    extras_require={'test':['pytest','pytest-watch', 'pytest-cov']},
+    entry_points={
+        'console_scripts':[
+            "trygrams = trygrams:main"
+            ]
+    }
+    )
