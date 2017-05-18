c2fc82244e8fe977fcf9797e595dc3e54d527935
Chris Hudson <c.ahudson84@yahoo.com>
2017-05-17 23:17:13 -0700

    two stupid equals...

--- src/trigrams.py
+++ src/trigrams.py
@@ -1,58 +1,69 @@
+"""."""
 import random
 import io
 import os
 import sys
 
 
-def main(input_body = 'body_text.txt', n = "21"):
+def main(input_body='body_text.txt', n="21"):
+    """."""
     if os.path.isfile(input_body):
         if os.stat(input_body).st_size >= 1:
             tri_dictionary = trigrams_build_dict(input_body)
-            n = int(n)
-            return trigrams_build_trigram(n, tri_dictionary)
+            if tri_dictionary:
+                n = int(n)
+                return trigrams_build_trigram(n, tri_dictionary)
+            return'\nThe file should have three or more words.\n'
         elif os.stat(input_body).st_size == 0:
-            return 'Bro, give me more words'
+            return '\nBro, give me more words\n'
     else:
         return 'Enter actual .txt file Dude'
 
 
-def trigrams_build_dict(input_body = 'body_text.txt'):
+def trigrams_build_dict(input_body='body_text.txt'):
+    """."""
     tri_dictionary = {}
     word_1 = ''
     word_2 = ''
     all_words_key = ''
     converted_unicode_text = io.open(input_body, encoding='utf-8')
     for text in converted_unicode_text:
-        for word in text.split():
-            if word_1 == '':
-                word_1 == word
-            elif word_2 == '':
-                word_2 == word
-            else:
-                all_words_key = word_1 + ' ' + word_2
-                if all_words_key in tri_dictionary:
-                    list_word_3 = tri_dictionary[all_words_key]
-                    list_word_3.append(word)
-                    tri_dictionary[all_words_key] = list_word_3
+        words_from_file = text.split()
+        if len(words_from_file) >= 3:
+            for word in words_from_file:
+                if word_1 == '':
+                    word_1 = word
+                elif word_2 == '':
+                    word_2 = word
                 else:
-                    tri_dictionary[all_words_key] = [word]
-                word_1 = word_2
-                word_2 = word
+                    all_words_key = word_1 + ' ' + word_2
+                    if all_words_key in tri_dictionary:
+                        list_word_3 = tri_dictionary[all_words_key]
+                        list_word_3.append(word)
+                        tri_dictionary[all_words_key] = list_word_3
+                    else:
+                        tri_dictionary[all_words_key] = [word]
+                    word_1 = word_2
+                    word_2 = word
+        elif len(words_from_file) < 3:
+            return
     return tri_dictionary
 
 
 def trigrams_random_int(x):
+    """."""
     from random import randint
     return randint(0, x - 1)
 
 
 def trigrams_build_trigram(n, tri_dictionary):
+    """."""
     rand_dict_keys = random.choice(list(tri_dictionary.keys()))
     split_dict_keys = rand_dict_keys.split()
     word_1 = split_dict_keys[0]
     word_2 = split_dict_keys[1]
     generated_result = split_dict_keys[0].capitalize()
-    generated_result = generated_result + ' ' + word_2
+    generated_result = generated_result + ' ' + split_dict_keys[1]
     for index in range(0, n - 2):
         all_words_key = word_1 + ' ' + word_2
         value_on_all_words_key = list(tri_dictionary[all_words_key])
